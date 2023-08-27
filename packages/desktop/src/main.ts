0eac623a04129a6ea9fe027e80e397f3396062d9
camilo-camargo <camilocamargo49@gmail.com>
2023-08-27 15:36:27 -0500

    Add desktop electron structure package.

---
+++ packages/desktop/src/main.ts
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from "electron"
+
+async function main(){
+  const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+    });
+	  win.loadURL("https://github.com/LearnOfficial/learn");
+  };
+
+  await app.whenReady();
+  createWindow();
+}
+
+main();
