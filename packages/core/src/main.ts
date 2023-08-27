dc97e05c0ea2fffed147f82e02b77d4b52fc4f0d
camilo-camargo <camilocamargo49@gmail.com>
2023-08-27 15:06:38 -0500

    Add project workpaces and core graphql server structure.

---
+++ packages/core/src/main.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+	type Query {
+		version: String
+	}	
+`
+
+const resolvers = {
+	Query: {
+		version: () => "0.0.1"
+	}
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+	listen: {port: 4000}
+});
+
+console.log(`Server ready at: ${url}`);
