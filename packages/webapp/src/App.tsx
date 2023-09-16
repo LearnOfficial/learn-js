62fbb063e3b3056edc415c728d98105e7279ed0d
camilo-camargo <camilocamargo49@gmail.com>
2023-09-15 21:39:49 -0500

    Add webapp sub package.

---
+++ packages/webapp/src/App.tsx
@@ -0,0 +1,16 @@
+import { StaticImage } from '@learn/ui';
+import { View } from 'react-native';
+
+export function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <StaticImage size={150} image="LearnLogo" />
+    </View>
+  );
+}
---
+++ packages/webapp/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: 'window'
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web'
+    },
+    extensions: ['.web.tsx', '.web.jsx', '.web.js', '.tsx', '.ts', '.js']
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' },
+      mainFields: ['module', 'main'],
+      resolveExtensions: ['.web.js', '.js', '.ts']
+    }
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
+});
