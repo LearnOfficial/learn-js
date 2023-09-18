5e9a3914925a08ab8d23673c1beb6c45a7915168
camilo-camargo <camilocamargo49@gmail.com>
2023-09-17 19:49:08 -0500

    [webapp] Add slider with august months.

--- packages/webapp/src/pages/NoteBoard/index.tsx
+++ packages/webapp/src/pages/NoteBoard/index.tsx
@@ -1,8 +1,9 @@
 import { Button, StaticImage, useAuth } from '@learn/ui';
-import { ColorValue, Pressable, View } from 'react-native';
+import { ColorValue, Pressable, Text, View } from 'react-native';
 import { SubjectContainer } from './components/SubjectContainer';
 import { SubjectItemsContainer } from './components/SubjectItemsContainer';
 import { useState } from 'react';
+import { Slider } from './components/slider';
 
 type ISubjectItems = {
   id: string;
@@ -10,7 +11,18 @@ type ISubjectItems = {
   backgroundColor: ColorValue;
 };
 
+function getDaysInMonth(month, year) {
+  var date = new Date(year, month, 1);
+  var days = [];
+  while (date.getMonth() === month) {
+    days.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  return days;
+}
+
 export function NoteBoard() {
+  const augustDays = getDaysInMonth(7, 2023);
   const [subjects, setSubjects] = useState<ISubjectItems[]>([
     {
       id: '1',
@@ -60,9 +72,58 @@ export function NoteBoard() {
         </SubjectContainer>
       </View>
 
-      <View style={{ flex: 2, backgroundColor: 'yellow' }}></View>
-      <View>
-        <Button title="logout" onPress={async () => await logOut()} />
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: '#F9FBF4',
+          paddingVertical: 20,
+          paddingHorizontal: 10
+        }}
+      >
+        <View>
+          <Text
+            style={{ fontFamily: 'lexend', fontWeight: '100', fontSize: 20 }}
+          >
+            Tasks
+          </Text>
+        </View>
+
+        <View>
+          <Slider
+            data={augustDays}
+            renderItem={({ item }: { item: Date }) => {
+              return (
+                <View
+                  style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 10,
+                    backgroundColor: '#EEEEE3',
+                    padding: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: 'lexend',
+                      color: 'black',
+                      fontWeight: '800'
+                    }}
+                  >
+                    {item
+                      .toLocaleDateString('en-US', { weekday: 'long' })
+                      .slice(0, 3)}
+                  </Text>
+                  <Text style={{ fontFamily: 'lexend', color: 'black' }}>
+                    {item.getDate()}
+                  </Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+        <View></View>
       </View>
     </View>
   );
