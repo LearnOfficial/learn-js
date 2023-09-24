771ec94827d7c84f8cbb2145dcf4ba940e5d1645
camilo-camargo <camilocamargo49@gmail.com>
2023-09-24 17:00:20 -0500

    [web] Resolve failures building the web page.

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'window'
-  },
   resolve: {
     alias: {
       'react-native': 'react-native-web'
